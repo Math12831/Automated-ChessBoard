d82a6b9169fcfdafa38d169f7a3c8ddabbfe95cb
Nicholas Pizzino <pizzaboy@iprimus.com.au>
2014-08-09 10:08:49 +0800

    Big Update
    Made changes when making the Checkers.c.

--- Checkers.h
+++ Checkers.h
@@ -1,5 +1,5 @@
 // Checkers.h
-// Version 1.0
+// Version 1.2
 // Includes basic function declerations
 // These functions can be called by the player and the AI only.
 
@@ -20,28 +20,45 @@
 #define MOVE_BACKWARD_RIGHT 3
 
 // Players - maximum and minimum of 2
-#define PLAYER_1 0
-#define PLAYER_2 1
+#define NONE 0
+#define PLAYER_1 1
+#define PLAYER_2 2
 
 #define TRUE 1
 #define FALSE 0
 
 // The game structure
-typedef struct _game { 
-  int currentTurn;
-  char checkersBoard[NUM_OF_ROWS][NUM_OF_COLUMNS];
+
+typedef struct _game * Game;
+typedef struct _action * Action;
+typedef struct _player * Player;
+
+typedef struct _game {
+    int currentTurn;
+    int checkersBoard[NUM_OF_ROWS][NUM_OF_COLUMNS];
+    Player player1;
+    Player player2;
 } game;
+
+// The player structure
+typedef struct _player {
+    int checkers;
+    int crownedCheckers;
+} player;
 
 // Simple action structure - may include more items later for chess
 typedef struct _action {
-   int actionCode;
+    int actionCode;
 } action;
+
+// Begin a-new!
+Game newGame (int gameNumber);
 
 // Game over. Literally.
 void disposeGame (Game g);
 
 // Make a move!
-void makeMove (Game g, action a);
+void makeMove (Game g, Action a, int row, int col);
 
 // Who is currently winning the game?
 int getMostCheckers (Game g);
@@ -63,6 +80,6 @@ int getCrownedCheckers (Game g, int player);
 
 // This is a big one. It will require me to find out a few things about the game.
 // Basically, is the move you have specified legal or illegal?
-int isLegalMove (Game g, action a);
+int isLegalMove (Game g, Action a);
 
 // Enjoy!
